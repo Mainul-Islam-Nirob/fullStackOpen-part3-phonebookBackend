607babb4e450c14a74c523feedb0e8ba81fdda70
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-02-10 17:54:47 +0600

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+// middleware
+app.use(express.json())
+app.use(express.urlencoded())
+
+
 let persons = [
     {
         "id": 1,
@@ -54,6 +59,22 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const generateId = () => (Math.random() * 10000).toFixed(0)
+
+    const { body } = request
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    console.log(person)
+    response.json(person)
 })
 
 const PORT = 3001
